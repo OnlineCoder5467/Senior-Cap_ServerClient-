dca449e6c313127d7e925d9c92c353469273da64
CSGroupee <126740330+CSGroupee@users.noreply.github.com>
2023-10-13 10:23:02 -0700

    Update app.py
    Pulls and pushes to S3

--- flasks/app.py
+++ flasks/app.py
@@ -1,16 +1,38 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 import json
 import random
+from flask_cors import CORS
+import requests
+
+##MUST UPLOAD AM IMAGE BEFORE PULLING FROM getUnverifiedXs WHEN RUNNING LOCALLY
+##HAVE NOT CONFIGURED getInitialData to run at server start
 
 load_dotenv()
 
-UPLOAD_FOLDER = '../imagebucket/'
-UNVERIFIED_BUCKET = '../imagebucket'
+UPLOAD_FOLDER = './imagebucket/'
+UNVERIFIED_BUCKET = './imagebucket'
+API_ENDPOINT = 'https://mt5t6im18f.execute-api.us-west-1.amazonaws.com/dev'
 
 app = Flask(__name__)
+CORS(app)
+
+data = []
+
+
+def getInitialData():
+  response = requests.get(API_ENDPOINT)
+  if response.status_code != 200:
+    return jsonify({"error": "Failed to retrieve images"}), 500
+  api_images = response.json()
+  data.clear()
+  for i in api_images:
+    data.append(i)
+  print(data)
+  return
+
 
 @app.route("/")
 def hello_world():
@@ -19,7 +41,7 @@ def hello_world():
 
 @app.get('/upload')
 def upload_get():
-    return '''
+  return '''
       <form action="/upload" method="post" enctype="multipart/form-data">
         <input type="file" name="file">
         <input type="submit" value="Upload">
@@ -30,8 +52,14 @@ def upload_get():
 @app.route("/upload", methods=["POST"])
 def upload_file():
   file = request.files["file"]
-  file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-  return "do not test this page"
+  S3_BUCKET_URL = f"https://seniorcapstone.s3.amazonaws.com/{file.filename}"
+  headers = {'Content-Type': file.content_type}
+  response = requests.put(S3_BUCKET_URL, headers=headers, data=file)
+  if response.status_code == 200:
+    getInitialData()
+    return "File uploaded successfully!"
+  else:
+    return f"Failed to upload: {response.text}"
 
 
 @app.route("/checkModelVersion")
@@ -44,52 +72,48 @@ def downloadModel():
   return "Downloading model #_____"
 
 
-#returns sample json of newly uploaded objects for admin review
-@app.route("/getUniverifiedXs")
-def getUnverifiedXs():
-    images = []
-    imageID = 0
-    
-    for image in os.listdir(UNVERIFIED_BUCKET):
-        atag = getTag()
-        images.append({
-            "id": imageID,
-            "imageUrl": f"http://localhost:8788/imagebucket/{image}",
-            "tag": atag
-        })
-        imageID += 1
-    response_data = {
-        "imageCount": len(images),
-        "images": images
-    }        
-    return jsonify(response_data)
-
-
 def getTag():
   if (random.randint(1, 2) == 1):
-     return "dog"
+    return "dog"
   return "cat"
-   
 
-#returns sample json of newly uploaded objects for admin review
+
+@app.route("/getUnverifiedXs")
+def getUnverifiedXs():
+  resonseObject = []
+  imageID = 0
+  for d in data:
+    atag = getTag()
+    confidence = 90 if atag in d else 10
+    resonseObject.append({
+        "id": imageID,
+        "imageUrl": d,
+        "tag": atag,
+        "confidence": confidence
+    })
+    imageID += 1
+  return jsonify(resonseObject)
+
+
 @app.route("/getUniverifiedYs")
 def getUnverifiedYs():
-    images = []
-    imageID = 0    
-    for image in os.listdir(UNVERIFIED_BUCKET):
-        atag = getTag()
-        images.append({
-            "id": imageID,
-            "imageUrl": f"http://localhost:8788/imagebucket/{image}",
-            "tag": atag
-        })
-        imageID += 1
-    return jsonify(images)
+  images = []
+  imageID = 0
+  for image in os.listdir(UNVERIFIED_BUCKET):
+    atag = getTag()
+    images.append({
+        "id": imageID,
+        "imageUrl":
+        f"https://replit.com/@jondooley87/flaskapp#imagebucket/{image}",
+        "tag": atag
+    })
+    imageID += 1
+  return jsonify(images)
 
 
 @app.route("/updateUnverified")
 def updateUnverified():
-   return "Success"
+  return "Success"
 
 
 @app.route("/signupAdmin")
@@ -109,7 +133,7 @@ def signoutAdmin():
 
 @app.route("/trainModel")
 def trainModel():
-  return "Training"   
+  return "Training"
 
 
 @app.route("/updateAndArchiveModel")
@@ -117,5 +141,51 @@ def updateAndArchiveModel():
   return "New Model Up"
 
 
-if __name__ == "__main__":
-  app.run()
+if __name__ == '__main__':
+  getInitialData()
+  app.run(host='0.0.0.0', port=81)
+'''THIS FUNCTION RETURNS A JSON WITH IMAGE COUNT AND ALL IMAGES FROM LOCAL FOLDER
+#returns sample json of newly uploaded objects for admin review
+@app.route("/getUnverifiedXs")
+def getUnverifiedXs():
+  images = []
+  imageID = 0
+  for image in os.listdir(UNVERIFIED_BUCKET):
+    atag = getTag()
+    images.append({
+        "id": imageID,
+        "imageUrl": url_for("imagebucket", filename=image),
+        "tag": atag
+    })
+    imageID += 1
+  response_data = {"imageCount": len(images), "images": images}
+  return jsonify(response_data)
+'''
+'''THIS FUNCTION RETRIEVES JSON OF IMAGES ONLY FROM LOCAL FOLDER
+@app.route("/getUnverifiedXs")
+def getUnverifiedXs():
+  response = requests.get(API_ENDPOINT)
+  if response.status_code != 200:
+    return jsonify({"error": "Failed to retrieve images"}), 500
+  api_images = response.json()
+  images = []
+  imageID = 0  # Start from 0    
+  for imageUrl in api_images:
+    atag = getTag()
+    images.append({"id": imageID, "imageUrl": imageUrl, "tag": atag})
+    imageID += 1  # Increment the imageID for the next iteration
+  return jsonify(images)
+'''
+'''#THIS FUNCTION SAVES SINGLE IMAGE TO LOCAL FOLDER
+@app.route("/upload", methods=["POST"])
+def upload_file():
+  file = request.files["file"]
+  #  file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+  res = requests.post(
+      #  'https://example.com/api/users',
+      'https://s3.amazonaws.com/seniorcapstone/{file.filename}'
+      #except Exception as e:
+      #  return "Did not work, try again later"
+  )
+  return "It worked"
+'''
